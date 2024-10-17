8b48f15246c08f0db1a0ff0a2b21d59d1a07b074
SvennieboyDev <Sven.Deijkers@gmail.com>
2024-10-17 07:30:21 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+#Tutorial gestart op 16 oktober
+
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+class MyGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+        self.cols = 1
+
+        self.inside = GridLayout()
+        self.inside.cols = 2
+
+        self.inside.add_widget(Label(text="Name: "))
+        self.name = TextInput(multiline=False)
+        self.inside.add_widget(self.name)
+
+        self.inside.add_widget(Label(text="Last Name: "))
+        self.lastName = TextInput(multiline=False)
+        self.inside.add_widget(self.lastName)
+
+        self.inside.add_widget(Label(text="Email: "))
+        self.email = TextInput(multiline=False)
+        self.inside.add_widget(self.email)
+
+        self.add_widget(self.inside)
+
+        self.submit = Button(text="Submit", font_size=40)
+        self.add_widget(self.submit)
+        self.submit.bind(on_press=self.pressed)
+
+
+class MyApp(App):
+    def build(self):
+        self.title = "Tutorial"
+        return MyGrid()
+
+if __name__ == "__main__":
+    MyApp().run()
