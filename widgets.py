2b799082b82ba962c5e4cdc943ed7d4d7d21b40c
SvennieboyDev <Sven.Deijkers@gmail.com>
2024-10-17 12:46:26 +0200

    Touch & Mouse Input Tutorial Complete
    17-10-2024

--- widgets.py
+++ widgets.py
@@ -2,26 +2,26 @@
 
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
 from kivy.uix.widget import Widget
-from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import ObjectProperty
 
-class MyGrid(Widget):
-    name = ObjectProperty(None)
-    email = ObjectProperty(None)
+class Touch(Widget):
+    btn = ObjectProperty(None)
 
-    def btn(self):
-        print("Name:", self.name.text, "email:", self.email.text)
-        self.name.text = ""
-        self.email.text = ""
+    def on_touch_down(self, touch):
+        print("Mouse Down", touch)
+        self.btn.opacity = 0.5
+    
+    def on_touch_move(self, touch):
+        print("Mouse Move", touch)
+    
+    def on_touch_up(self, touch):
+        print("Mouse UP", touch)
+        self.btn.opacity = 1
 
 class MyApp(App):
     def build(self):
-        return MyGrid()
+        return Touch()
 
 if __name__ == "__main__":
     MyApp().run()
